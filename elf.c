4593d999bf2762f3f702a31123add247c4a71045
ColumPaget <colums.projects@gmail.com>
2022-08-02 23:23:35 +0100

    Better file selection for multiarch linux apps.

---
+++ elf.c
@@ -0,0 +1,22 @@
+#include "elf.h"
+
+int ELFFileGetBitWidth(const char *Path)
+{
+STREAM *S;
+char HDR[10];
+int result, RetVal=0;
+
+S=STREAMOpen(Path, "r");
+if (S)
+{
+	result=STREAMReadBytes(S, HDR, 5);
+	if ((result > 4) && (memcmp(HDR+1, "ELF", 3)==0) )
+	{
+		if (HDR[4]==1) RetVal=32;
+		else if (HDR[4]==2) RetVal=64;
+	}
+STREAMClose(S);
+}
+
+return(RetVal);
+}
---
+++ elf.h
@@ -0,0 +1,9 @@
+#ifndef SOMMELIER_ELF_H
+#define SOMMELIER_ELF_H
+
+#include "common.h"
+
+int ELFFileGetBitWidth(const char *Path);;
+
+
+#endif
